3afb2218f37138fcb5c2dfa4bc4deae5d1ec530f
Rpallas1 <rpallas1@asu.edu>
2025-02-09 13:09:28 -0700

    re-add image

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,15 @@
+echo "--- Building Project ---"
 
+yarn build
+
+echo "--- Project Built ---"
 echo "--- Syncing with S3 Bucket ---"
 
-aws s3 sync . s3://pallascreations.com --exclude ".git/*" --exclude ".DS_Store" --delete --exclude "deploy.sh" --exclude ".vscode/*"
-output=$(aws cloudfront create-invalidation --distribution-id E2C690930API5P --paths "/*")
+aws s3 sync ./dist s3://musicnotes.pallascreations.com --delete
+
+cloudfront_id=E1DZUXHF10LZKS 
+
+output=$(aws cloudfront create-invalidation --distribution-id $cloudfront_id --paths "/*")
 id=$(echo $output | jq -r ".Invalidation.Id")
 status=$(echo $output | jq -r ".Invalidation.Status")
 
@@ -12,7 +19,7 @@ echo "Status: $status"
 while [ $(echo $output | jq -r ".Invalidation.Status") != "Completed" ]
 do
     sleep 5
-    output=$(aws cloudfront get-invalidation --distribution-id E2C690930API5P --id $id)
+    output=$(aws cloudfront get-invalidation --distribution-id $cloudfront_id --id $id)
     status=$(echo $output | jq -r ".Invalidation.Status")
     echo "Status: $status"
 done
