80514f9bb31724df85814ae4400f8870bca61c15
Rpallas1 <rpallas1@asu.edu>
2025-02-09 10:11:41 -0700

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Music Notes</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
